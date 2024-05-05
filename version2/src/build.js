d7a2ad7b86e2edce404689398616ef05be751499
Andrew <oleshkoa@gmail.com>
2024-05-05 17:04:42 -0400

    Add eslint ts plugin

--- version2/src/build.js
+++ version2/src/build.js
@@ -1,20 +1,27 @@
-const { build } = require("esbuild");
+const { build } = require('esbuild');
+const { esbuildPluginTsc } = require('esbuild-plugin-tsc');
 const { dependencies, peerDependencies } = require('./package.json');
+//const { Generator } = require('npm-dts'); // requries yarn add npm-dts --dev
 
-const sharedConfig = {
+// new Generator({
+//   entry: 'src/index.ts',
+//   output: 'dist/index.d.ts',
+// }).generate();
+
+build({
   entryPoints: ["src/index.ts"],
+  outfile: "dist/index.js",
   bundle: true,
   minify: false,
   // only needed if you have dependencies
   // external: Object.keys(dependencies).concat(Object.keys(peerDependencies)),
   sourcemap: true,
- 
-};
-
-build({
-  ...sharedConfig,
   platform: 'browser',
   format: 'esm',
-  outfile: "dist/index.js",
-  
+  plugins: [
+    esbuildPluginTsc({
+      force: true,
+      //tsconfigPath: './tsconfig.json'
+    }),
+  ],
 });
